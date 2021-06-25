42b86b1cfbb207b55ebb13f6330bf35e0f0027f3
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-06-26 00:40:27 +0300

    Qualifier Literal, Event Qualifier

--- cditest/src/main/java/com/godoro/cditest/event/MyEventBean.java
+++ cditest/src/main/java/com/godoro/cditest/event/MyEventBean.java
@@ -15,9 +15,22 @@ public class MyEventBean {
 	@Inject
 	private Event<MyEventData> event;		//MyEventData şeklinde event aldık.
 	
+	@Inject
+	private @MyEventQualifier(level=2) Event<MyEventData> event2;
+	
+	@Inject
+	private @MyEventQualifier(level=4) Event<MyEventData>	event4;
 	
 	public void perform() {
-		event.fire(new MyEventData("Genel olay..."));	//Burda o eventi, fire ettik. Yani olayı başlattık.
+		event.fire(new MyEventData("Genel event..."));	//Burda o eventi, fire ettik. Yani olayı başlattık.
+	
+		event2.fire(new MyEventData("event 2 ....")); //Fırlattım  -> MyEventListener'da yakala.
+		
+		event4.fire(new MyEventData("event 4...."));
+		
+		
+		event.select(new MyEventQualifierLiteral(2))
+			 .fire(new MyEventData(" Qualifier event 2 ...."));
 	}
 	
 }
--- cditest/src/main/java/com/godoro/cditest/event/MyEventListener.java
+++ cditest/src/main/java/com/godoro/cditest/event/MyEventListener.java
@@ -4,10 +4,17 @@ import javax.enterprise.event.Observes;
 
 public class MyEventListener {
 	
-	
+	// GENERAL
 	public void listen(@Observes   MyEventData myEventData) {
 		System.out.println("Event  "+myEventData.getMyField());
 	}
-
+	
+	// LEVEL 2
+	public void listen2(@Observes @MyEventQualifier(level=2) MyEventData myEventData) {
+		System.out.println("Event level 2 "+myEventData.getMyField());
+	}
+	
+	
+	
 }
  
